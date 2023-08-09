219d7d2fb1b6f1bd60062131d37faf68df137aa3
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-09 09:32:06 -0700

    documented

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""
+Method that determines if all the boxes can be opened.
+"""
+
 
 def canUnlockAll(boxes):
+    """ determines if all the boxes can be opened"""
     if not boxes:
         return False
 
