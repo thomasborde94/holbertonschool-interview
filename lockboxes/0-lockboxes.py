9bec4b6e9f6c566263996f3df0a386a8fa100314
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-09 09:29:08 -0700

    task 0

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,16 +5,20 @@ def canUnlockAll(boxes):
         return False
 
     boxNumber = len(boxes)
-    unlocked_boxes = [0] # Contains unlocked boxes, initialized with box 0
+    unlocked_boxes = [0]  # Contains unlocked boxes, initialized with box 0
 
     keys_to_check = boxes[0]  # Initialize with keys in the first box
 
-    while keys_to_check: # while there are keys to open boxes
+    while keys_to_check:  # while there are keys to open boxes
         new_keys = []
-        for key in keys_to_check: # check for each key in the keys we have
-            if key < boxNumber and key not in unlocked_boxes: # check if the key is valid and we dont already opened the box of the key
-                unlocked_boxes.append(key) # append the corresponding box to the unlocked boxes
-                new_keys.extend(boxes[key]) # adds the key to all the keys we have
-        keys_to_check = new_keys # update the keys to check
+        for key in keys_to_check:  # check for each key in the keys we have
+            if key < boxNumber and key not in unlocked_boxes:  # check if
+                # the key is valid and we did not already open the box
+                unlocked_boxes.append(key)  # append the corresponding
+                # box to the unlocked boxes
+                new_keys.extend(boxes[key])  # adds the key to all the
+                # keys we have
+        keys_to_check = new_keys  # update the keys to check
 
-    return len(unlocked_boxes) == boxNumber # check if we opened as many boxes as there are boxes
+    return len(unlocked_boxes) == boxNumber  # check if we opened as many
+# boxes as there are boxes
