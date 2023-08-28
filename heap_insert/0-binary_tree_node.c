05801fec31d486ad3873827888163a12b171119f
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-28 11:52:37 +0000

    betty doc

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -6,10 +6,12 @@
  *
  * @parent: Pointer to the parent of the node to create
  * @value: Value of the node to create
+ * Return: pointer to the new node of Null
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
   binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+
   if (newNode == NULL)
     return (NULL);
 
