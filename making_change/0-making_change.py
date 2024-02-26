29631c0e1c58d616519a15a33c32bf584086dec3
thomasborde94 <thomas.borde94@hotmail.fr>
2024-02-26 10:07:32 +0100

    trying new solution

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
-"""
-Given a pile of coins of different values,
-determine the fewest number of coins needed to
-meet a given amount total
-"""
+"""Making change O(n)"""
 
 
 def makeChange(coins, total):
-    """
-    Return: number of coins needed to meet total
-        - If total is 0 or less, return 0
-        - If total cannot be met by any number of coins you have, return -1
-        - Coins is a list of the values of the coins in your possession
-        - The value of a coin will always be an integer greater than 0
-        - You can assume you have an infinite number of each denomination of
-        coin in the list
-    """
+    """Clasic Bottom-Up dynamic programming"""
     if total <= 0:
         return 0
-
-    coins.sort(reverse=True)
-    sum = 0
-    i = 0
-    counter = 0
-    num_coins = len(coins)
-    while sum < total and i < num_coins:
-        while coins[i] <= total - sum:
-            sum += coins[i]
-            counter += 1
-            if sum == total:
-                return counter
-        i += 1
-    return -1
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    coins.sort()
+    for i in range(total + 1):
+        for coin in coins:
+            if coin > i:
+                break
+            if dp[i - coin] != -1:
+                dp[i] = min(dp[i - coin] + 1, dp[i])
+    return dp[total] if dp[total] != float('inf') else -1
